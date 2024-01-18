6ad4e784669dcb1a556bbb3b61c5e3e1b68fff34
Ruben Ocasio <rocasio@rubenocasio.com>
2024-01-18 11:29:39 -0500

    Updates

---
+++ cart/admin.py
@@ -0,0 +1,20 @@
+from django.contrib import admin
+from .models import ColorVariation, Product, OrderItem, Order, SizeVariation, Address
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = [
+        'address_line_1',
+        'address_line_2',
+        'city',
+        'zip_code',
+        'address_type',
+    ]
+
+
+admin.site.register(Address, AddressAdmin)
+admin.site.register(ColorVariation)
+admin.site.register(Product)
+admin.site.register(OrderItem)
+admin.site.register(Order)
+admin.site.register(SizeVariation)
